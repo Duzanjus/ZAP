27dc77b74c62755ee6f59f829f7ebe2d91d0d563
Duzanjus <78372160+Duzanjus@users.noreply.github.com>
2021-02-02 23:38:10 -0300

    Add files via upload

---
+++ plugins/rules.js
@@ -0,0 +1,31 @@
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+  conn.reply(m.chat, `
+Bot de Termos e Condições * ANJOS *
+1. O texto e seu nome de usuário do WhatsApp serão armazenados no servidor enquanto o bot estiver ativo
+2. Os dados serão excluídos quando o bot estiver offline
+3. Não armazenamos fotos, vídeos, arquivos, áudios e documentos que você envia
+4. Nunca pediremos que você forneça informações pessoais
+5. Se você encontrar um bug / erro, informe-o diretamente ao proprietário do bot
+6. O que quer que você peça neste bot, NÃO SEREMOS RESPONSÁVEIS!
+
+obrigado !
+2021©Drawl Nag
+`.trim(), m)
+}
+handler.help = ['rules']
+handler.tags = ['info']
+handler.command = /^(rules)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
+
+
